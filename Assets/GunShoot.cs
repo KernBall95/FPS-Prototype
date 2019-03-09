1333e267eae7878b3e742f8a12224f99125148cb
Kern Ball <kern.ball@yahoo.co.uk>
2019-03-09 18:06:02 +0000

    Initial Commit
    Basic Raycast shooting in place. Test targets can detect being shot, have a set amount of hp and can be destroyed.

---
+++ Assets/GunShoot.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunShoot : MonoBehaviour {
+
+    public Transform shootOrigin;
+    public int damage = 1;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Shoot();
+	}
+
+    void Shoot()
+    {
+        if (Input.GetButtonDown("Fire1")) {
+            RaycastHit hit;
+            
+            if(Physics.Raycast(shootOrigin.position, transform.TransformDirection(Vector3.forward), out hit, Mathf.Infinity))
+            {
+                Debug.DrawRay(shootOrigin.position, transform.TransformDirection(Vector3.forward) * hit.distance, Color.yellow);
+                Debug.Log("Hit!");
+
+                hit.collider.SendMessageUpwards("ApplyDamage", damage, SendMessageOptions.DontRequireReceiver);
+            }
+            else
+            {
+                Debug.DrawRay(shootOrigin.position, transform.TransformDirection(Vector3.forward) * 1000, Color.red);
+                Debug.Log("Did not hit.");
+            }
+        }
+    }
+}
---
+++ Assets/TestTarget.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestTarget : MonoBehaviour {
+
+    public int hitPoints = 3;
+    Renderer rend;
+
+    void Start()
+    {
+        rend = GetComponent<Renderer>();
+        rend.material.color = Color.green;
+    }
+
+    void ApplyDamage(int damage)
+    {
+        if (hitPoints <= 0)
+            return;
+
+        hitPoints -= damage;
+
+        if (hitPoints == 2)
+            rend.material.color = Color.yellow;
+        else if (hitPoints == 1)
+            rend.material.color = Color.red;
+
+        if (hitPoints <= 0) 
+            Die();
+    }
+
+    void Die()
+    {
+        Destroy(gameObject);
+    }
+}
