7e81917c974f2766e0aaef2ff981e25bbcde2372
Kern Ball <kern.ball@yahoo.co.uk>
2019-04-04 03:26:16 +0100

    Downloaded Gun Assets
    I downloaded some free gun assets from the Unity Asset store, which included models, animations and some sounds. I then implemented then so that the animations and the sounds play when they should.
    
    Only working with the pistol right now. Will implement more weapons in the near future.

--- Assets/Scripts/GunShoot.cs
+++ Assets/Scripts/GunShoot.cs
@@ -6,6 +6,10 @@ public class GunShoot : MonoBehaviour {
 
     public Transform shootOrigin;
     public GameObject hitParticles;
+    public GameObject heldGun;
+
+    Animation gunAnim;
+    AudioSource audio;
 
     public int damage = 1;
 
@@ -15,56 +19,72 @@ public class GunShoot : MonoBehaviour {
     public float explosionPower;
     public float explosionRadius = 20f;
 
+    public float fireRate = 1.0f;
+    public float currentFireTimer = 0.0f;
+
     Vector3 explosionPos;
     Collider[] colliders;
 
 	// Use this for initialization
 	void Start () {
-		
+        gunAnim = heldGun.GetComponent<Animation>();
+        gunAnim.wrapMode = WrapMode.Once;
+
+        audio = heldGun.GetComponent<AudioSource>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
+        currentFireTimer -= Time.deltaTime;
+        if (currentFireTimer < 0.0f)
+            currentFireTimer = 0.0f;
         Shoot();
 	}
 
     void Shoot()
     {
         if (Input.GetButtonDown("Fire1")) {
-            RaycastHit hit;
-            
-            if(Physics.Raycast(shootOrigin.position, transform.TransformDirection(Vector3.forward), out hit, Mathf.Infinity))
+            if (currentFireTimer <= 0.0f)
             {
-                force = shootOrigin.forward * forceStrength;
+                currentFireTimer = fireRate;
+                gunAnim.Play();
+                audio.Play();
+                RaycastHit hit;
 
-                Debug.DrawRay(shootOrigin.position, transform.TransformDirection(Vector3.forward) * hit.distance, Color.yellow);
-                Debug.Log("Hit!");
-
-                hit.collider.SendMessageUpwards("ApplyDamage", damage, SendMessageOptions.DontRequireReceiver);
-
-                if (hit.rigidbody)
-                    hit.rigidbody.AddForceAtPosition(force, hit.point);
-                else
+                if (Physics.Raycast(shootOrigin.position, transform.TransformDirection(Vector3.forward), out hit, Mathf.Infinity))
                 {
-                    explosionPos = hit.point;
-                    colliders = Physics.OverlapSphere(explosionPos, explosionRadius);
-                    foreach(Collider affected in colliders)
-                    {
-                        Rigidbody rb = affected.GetComponent<Rigidbody>();
+                    force = shootOrigin.forward * forceStrength;
 
-                        if (rb != null)
-                            rb.AddExplosionForce(explosionPower, explosionPos, explosionRadius, 1.5f);
+                    Debug.DrawRay(shootOrigin.position, transform.TransformDirection(Vector3.forward) * hit.distance, Color.yellow);
+                    Debug.Log("Hit!");
+
+                    hit.collider.SendMessageUpwards("ApplyDamage", damage, SendMessageOptions.DontRequireReceiver);
+
+                    if (hit.rigidbody)
+                        hit.rigidbody.AddForceAtPosition(force, hit.point);
+                    else
+                    {
+                        explosionPos = hit.point;
+                        colliders = Physics.OverlapSphere(explosionPos, explosionRadius);
+                        foreach (Collider affected in colliders)
+                        {
+                            Rigidbody rb = affected.GetComponent<Rigidbody>();
+
+                            if (rb != null)
+                                rb.AddExplosionForce(explosionPower, explosionPos, explosionRadius, 1.5f);
+                        }
                     }
+
+                    Vector3 incomingVec = hit.point - shootOrigin.position;
+                    Vector3 reflectVec = Vector3.Reflect(incomingVec, hit.normal);
+                    GameObject clone = Instantiate(hitParticles, hit.point, Quaternion.Euler(reflectVec));
                 }
 
-                Vector3 incomingVec = hit.point - shootOrigin.position;
-                Vector3 reflectVec = Vector3.Reflect(incomingVec, hit.normal);
-                GameObject clone = Instantiate(hitParticles, hit.point, Quaternion.Euler(reflectVec));
-            }
-            else
-            {
-                Debug.DrawRay(shootOrigin.position, transform.TransformDirection(Vector3.forward) * 1000, Color.red);
-                Debug.Log("Did not hit.");
+                else
+                {
+                    Debug.DrawRay(shootOrigin.position, transform.TransformDirection(Vector3.forward) * 1000, Color.red);
+                    Debug.Log("Did not hit.");
+                }
             }
         }
     }
